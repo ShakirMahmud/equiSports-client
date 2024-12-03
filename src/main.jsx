7e7fbc0d29d462509c835917feec567cc97f08d1
ShakirMahmud <shakirmahmud50@gmail.com>
2024-12-03 19:43:00 +0600

    firebase installed and react router and AuthContext setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { router } from './routers/router'
+import { RouterProvider } from 'react-router-dom'
 // import App from './App.jsx'
 
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Hello world!!</div>,
-  },
-]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
