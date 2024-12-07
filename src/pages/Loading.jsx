5b3ea1cb35185fef1370a520691ece148288f532
ShakirMahmud <shakirmahmud50@gmail.com>
2024-12-07 23:46:44 +0600

    Final commit but need some polishing

--- src/pages/Loading.jsx
+++ src/pages/Loading.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Loading = () => {
     return (
-        <div className='flex min-h-[70vh] justify-center items-center dark:bg-slate-800'>
+        <div className='flex min-h-screen justify-center items-center dark:bg-slate-800'>
             <span className="loading loading-spinner text-primary"></span>
             <span className="loading loading-spinner text-secondary"></span>
             <span className="loading loading-spinner text-accent"></span>
